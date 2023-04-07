a1389d7f1605e1f965175cca723b16523dfeecc3
Mohit <mohitsharma20036396@gmail.com>
2023-04-07 07:12:31 +0530

    table version 1

---
+++ Table.js
@@ -0,0 +1,9 @@
+function Table() {
+
+    return <h1>Table </h1>
+
+  }
+  
+  const root = ReactDOM.createRoot(document.getElementById('container'));
+  const element = <Table name="Sara" />;
+  root.render(element);
